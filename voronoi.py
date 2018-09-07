75a7e3731e45bacd0f62c360ae269af74e194b2b
David Alexander <david.alexander@etu.unige.ch>
2018-09-07 19:45:24 +0200

    Add files via upload

--- voronoi.py
+++ voronoi.py
@@ -3,6 +3,7 @@ from scipy.spatial import Voronoi
 from finite_voronoi import voronoi_finite_polygons_2d
 from landmarks import land2coords, facial_landmarks
 import os
+import sys
 from skimage import io, color
 from skimage.draw import polygon, polygon_perimeter
 from random import randint
@@ -13,29 +14,49 @@ from datetime import datetime
 from skimage.exposure import equalize_adapthist
 from collections import Counter
 from skimage.color import rgb2lab,lab2rgb
+from skimage.segmentation import mark_boundaries, slic
+
+
 
 
 # Import image
-load_path = os.path.join(os.getcwd(), 'Images', 'falcone.jpg');
-save_path = os.path.join(os.getcwd(), 'Images', 'falconeresult.jpg');
+load_path = sys.argv[1]
+facial_detection = sys.argv[2]
+if len(sys.argv) > 3:
+    npoints = sys.argv[3]
+
+save_path = "FinalResultDavid.jpg"
 img = io.imread(load_path)
 
-# Retrieve facial landmarks
-points = facial_landmarks(img)
-minx = np.amin(points[:,0])
-miny = np.amin(points[:,1])
-maxx = np.amax(points[:,0])
-maxy = np.amax(points[:,1])
-img = img[minx:maxx,miny:maxy]
+if not facial_detection :
+    x = np.random.randint(img.shape[0], size=npoints)
+    y = np.random.randint(img.shape[1], size=npoints)
+    points = np.zeros((npoints,2), dtype=int)
+    points[:,0] = x
+    points[:,1] = y
 
-# rescale points coordinates
-n = points.shape[0]
-for i in range(0,n):
-    points[i,0] = points[i,0] - minx
-    points[i,1] = points[i,1] - miny
+else :
+    # Retrieve facial landmarks
+    points = facial_landmarks(img)
+    minx = np.amin(points[:,0])
+    miny = np.amin(points[:,1])
+    maxx = np.amax(points[:,0])
+    maxy = np.amax(points[:,1])
+    img = img[minx:maxx,miny:maxy]
+
+    # rescale points coordinates
+    n = points.shape[0]
+    for i in range(0,n):
+        points[i,0] = points[i,0] - minx
+        points[i,1] = points[i,1] - miny
 
 # Compute Voronoi partition
-vor = Voronoi(points)
+vor = Voronoi(points,incremental = 1)
+vertices = vor.vertices
+regions = vor.regions
+
+
+# compute finite voronoi
 regions, vertices = voronoi_finite_polygons_2d(vor)
 
 # Colorize the picture
@@ -65,7 +86,6 @@ for region in regions:
     # colorize the perimeter
     rr, cc = polygon_perimeter(poly[:, 0], poly[:, 1], img.shape)
     img[rr, cc] = (0, 0, 0)
-
 
 # Back to rgb and save
 img = lab2rgb(img)
